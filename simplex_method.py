0ef5091962ab1ac05cbb2938315b676248c5fb00
Anastasiia-prog <57684659+Anastasiia-prog@users.noreply.github.com>
2020-11-10 21:40:15 +0300

    Add files via upload

--- simplex_method.py
+++ simplex_method.py
@@ -64,7 +64,96 @@ def kan_form(cond):
     return n
 
 # 2) Составляем симплекс-таблицу
-def simplex_table(cond, eq_cond, minmax):
+n = 4
+# Создаем таблицу нужного размера и заполняем нулями
+def get_zero_table(cond, n):
+    table_zero = [[0] * (len(cond) + n) for i in range(len(cond) + 1)]
+    return table_zero
+
+print('table_zero:', get_zero_table(cond, n))
+table = get_zero_table(cond, n)
+
+def simplex_table(cond, eq_cond, minmax, table):
+    c = 0
+    # Добавляем ограничения в таблицу
+    for i in range(len(table)):
+        for j in range(len(table[i])):
+            if i < len(cond) and j < len(cond[i]):
+                table[i][j] = cond[i][j]
+            else: # Добавляем коэффициенты при добавочных переменных
+                if c < len(kan_form(cond)):
+                    table[i][j] = kan_form(cond)[c]
+                    i += 1
+                    c += 1
+    # Добавляем свободные члены
+    for i in range(len(eq_cond)):
+        table[i][-1] = eq_cond[i]
+    # Добавляем коэффициенты при целевой функции
+    if minmax == 'MAX':
+        for i in range(len(func)):
+            table[-1][i] = -func[i]
+    elif minmax == 'MIN':
+        for i in range(len(func)):
+            table[-1][i] = func[i]
+    return table
+
+print('simplex table:', simplex_table(cond, eq_cond, minmax, table))
+
+# 3) Выбираем столбец с наименьшей оценкой
+def find_min_column():
+    global min_est, index_min
+    if minmax == 'MAX':
+        min_est = min(simplex_table(cond, eq_cond, minmax, table)[-1])
+        index_min = min(range(len(simplex_table(cond, eq_cond, minmax, table)[-1])), key=simplex_table(cond, eq_cond, minmax, table)[-1].__getitem__)
+
+    return min_est, index_min
+
+print('Наименьшая оценка:', find_min_column()[0], 'Индекс столбца с наименьшей оценкой:', find_min_column()[1])
+
+# 4) Исходя из пункта 3 выбираем разрешающий элемент:
+def perm_element():
+    lst_for_find_perm_el = []
+    current_table = simplex_table(cond, eq_cond, minmax, table)
+    for i in range(len(current_table) - 1):
+        lst_for_find_perm_el.append(current_table[i][-1]/current_table[i][find_min_column()[1]])
+    for_perm_el = min(lst_for_find_perm_el)
+    for i in range(len(lst_for_find_perm_el)):
+        if lst_for_find_perm_el[i] == for_perm_el:
+            ind_perm_el = i
+    perm_el = simplex_table(cond, eq_cond, minmax, table)[ind_perm_el][find_min_column()[1]]
+    # найдем индекс столбца разрешающего элемента
+    row_perm_el = min(range(len(lst_for_find_perm_el)),
+                    key=lst_for_find_perm_el.__getitem__)
+    return perm_el, row_perm_el
+
+print('Разрешающий элемент:', perm_element()[0], 'Индекс строки разрешающего элемента:', perm_element()[1])
+
+# 5) Перерасчитываем значения симплекс-таблицы и разрешающего элемента,
+# пока в последней строке есть отрицательные элементы
+from math import fabs
+
+def recalculation_simplex_table_for_max():
+    current_table = simplex_table(cond, eq_cond, minmax, table)
+    perm_el = perm_element()[0]
+    row_ind_perm = perm_element()[1]
+    column_ind_perm = find_min_column()[1]
+    ###
+    fabs_values_from_table = [fabs(i) for i in current_table[-1]]
+    ###
+    while max(current_table[-1]) < max(fabs_values_from_table):
+        for j in range(len(current_table[row_ind_perm])):
+            current_table[row_ind_perm][j] = current_table[row_ind_perm][j] / perm_el
+        for i in range(len(current_table)):
+            for j in range(len(current_table[i])):
+                current_table[i][j] = current_table[i][j] - (current_table[row_ind_perm][j] * current_table[i][column_ind_perm]) / perm_el
+
+    return current_table
+print('')
+print('Перерасчет симплекс-таблицы:')
+print(recalculation_simplex_table_for_max())
+###############
+
+def old_simplex_table(cond, eq_cond, minmax):
     find_n = kan_form(cond)
     table = [cond[i] + [0] * i + [find_n[i]] + [eq_cond[i]] for i in range(len(find_n))]
     if minmax == 'MAX':
@@ -80,54 +169,9 @@ def simplex_table(cond, eq_cond, minmax):
     while count != len(find_n):
         table[-1].append(0)
         count += 1
+    ###
+    table[0][4] = 0
+    table[0].append(12)
+    ###
     return table
 
-# 3) Выбираем столбец с наименьшей оценкой
-def find_min_column():
-    global min_est, index_min
-    if minmax == 'MAX':
-        min_est = min(simplex_table(cond, eq_cond, minmax)[-1])
-        index_min = min(range(len(simplex_table(cond, eq_cond, minmax)[-1])), key=simplex_table(cond, eq_cond, minmax)[-1].__getitem__)
-
-    return min_est, index_min
-
-# 4) Исходя из пункта 3 выбираем разрешающий элемент:
-def perm_element():
-    lst_for_find_perm_el = []
-    table = simplex_table(cond, eq_cond, minmax)
-    for i in range(len(table) - 1):
-        lst_for_find_perm_el.append(table[i][-1]/table[i][find_min_column()[1]])
-    for_perm_el = min(lst_for_find_perm_el)
-    for i in range(len(lst_for_find_perm_el)):
-        if lst_for_find_perm_el[i] == for_perm_el:
-            ind_perm_el = i
-    perm_el = simplex_table(cond, eq_cond, minmax)[ind_perm_el][find_min_column()[1]]
-    # найдем индекс столбца разрешающего элемента
-    row_perm_el = min(range(len(lst_for_find_perm_el)),
-                    key=lst_for_find_perm_el.__getitem__)
-    return perm_el, row_perm_el
-
-# 5) Перерасчитываем значения симплекс-таблицы и разрешающего элемента,
-# пока в последней строке есть отрицательные элементы
-from math import fabs
-
-def recalculation_simplex_table_for_max():
-    table = simplex_table(cond, eq_cond, minmax)
-    perm_el = perm_element()[0]
-    row_ind_perm = perm_element()[1]
-    column_ind_perm = find_min_column()[1]
-    ###
-    fabs_values_from_table = [fabs(i) for i in table[-1]]
-    ###
-    while max(table[-1]) < max(fabs_values_from_table):
-        for j in range(len(table[row_ind_perm])):
-            table[row_ind_perm][j] = table[row_ind_perm][j] / perm_el
-        for i in range(len(table)):
-            if i == row_ind_perm:
-                i += 1
-            for j in range(len(table[i])):
-                table[i][j] = table[i][j] - (table[row_ind_perm][j] * table[i][column_ind_perm]) / perm_el
-
-    return table
-
-print(recalculation_simplex_table_for_max())
